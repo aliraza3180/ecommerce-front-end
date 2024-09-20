7fb374aa33965633923a46501c64609548b80d17
aliraza3180 <161782568+aliraza3180@users.noreply.github.com>
2024-09-20 22:58:31 +0500

    Update LatestCollection.jsx

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -14,8 +14,10 @@ const LatestCollection = () => {
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
         <Title text1={"LATEST"} text2={" COLLECTIONS"} />
-        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600"></p>
-      </div>
+        <p className="w-3/4 text-xs sm:text-sm md:text-base m-auto text-gray-600">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the.
+        </p>      </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
         {latestProducts.map((item, index) => (
           <ProductItem
